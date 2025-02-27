fab2573eedd9aa2fafda3b9356b413d01e1072b7
kynth65 <kynth65@gmail.com>
2025-02-27 15:42:52 +0800

    update

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -15,8 +15,7 @@ const ProductCard = ({ product, onClick }) => {
           </span>
         </div>
       )}
-
-      {/* Image container with colored background */}
+      ]{" "}
       <div className="pt-6 px-6 pb-2 bg-gradient-to-b from-emerald-50/50 to-white">
         <img
           src={product.image}
@@ -24,7 +23,6 @@ const ProductCard = ({ product, onClick }) => {
           className="h-40 w-full object-contain mx-auto transition-transform duration-300 group-hover:scale-105"
         />
       </div>
-
       {/* Content */}
       <div className="p-4">
         <div className="flex justify-between items-start">
